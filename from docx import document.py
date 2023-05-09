28dab296a1d483790c2bad3b4fe1ca77b3863791
enedelte <daalejoar@gmail.com>
2023-05-08 22:42:33 -0500

    -m "se crea nuevo archivo base modelo automatizacion"

---
+++ from docx import document.py
@@ -0,0 +1,3 @@
+from docx import Document
+document = Document('C:\Users\daale\OneDrive - NUEVA EPS\GO_PQR\querys\mi_proforma_auto.docx')
+document.save('mi_proforma_auto.docx')
--- midocumentoauto.py
+++ midocumentoauto.py
@@ -1,1 +1,20 @@
-import 
+# Inportar libreria DOCX
+from docx import Document
+# Esta llave es el documento nuevo creado
+document = Document()
+# Crea titulo 1
+document.add_heading("Hola mundo")
+# (, Level=2; Level=3) es el separador en titulos heading
+document.add_heading("este es mi primer mensaje", level=2)
+document.add_heading("para todos", level=3)
+paragraph = document.add_paragraph("este es otro mensaje que envio")
+paragraph.add_run(" y esta letra está en negrilla.").bold = True
+paragraph.add_run(" y esta letra está en italica.").italic = True
+# document.add_page_break divide páginas
+document.add_page_break()
+
+# Se guarda documento con el nombre ("")
+document.save("prueba1.docx") 
+
+
+#print(Document)
